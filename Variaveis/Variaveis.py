b56afe591f8f743466a28ecf0f40fb7e6922293f
Carlos Henrique <carloscops8@gmail.com>
2026-01-14 21:38:30 -0300

    Adiciona Exemplos De Variáveis Em Python

---
+++ Variaveis/Variaveis.py
@@ -0,0 +1,9 @@
+nome = "Carlos Henrique"
+idade = 20
+altura = 1.75
+estudante_de_programação= True
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Estudando TI:", estudante_de_programação)
